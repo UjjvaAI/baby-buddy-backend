354f35e4d9524a6bbfa3e61086401e24dae8a818
UjjvaAI <ujjvastore@gmail.com>
2025-07-24 16:37:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,4 +30,8 @@ app.post("/create-order", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
